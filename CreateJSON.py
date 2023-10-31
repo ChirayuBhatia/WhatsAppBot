392ddf6c38dabdef613c2c9bd28ab2186d271b91
ChirayuBhatia <bhatiachirayu@gmail.com>
2023-10-31 09:54:54 +0530

    Created .enf file
    Creating library for different messages.

--- CreateJSON.py
+++ CreateJSON.py
@@ -2,48 +2,63 @@ import json
 
 
 def text_message(phone_number: int, message_content: str, preview_url: bool = False):
-    mess = \
-        {
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": f"91{phone_number}",
-            "type": "text",
-            "text": {
-                "preview_url": preview_url,
-                "body": message_content
-            }
+    return json.dumps({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": f"+91{phone_number}",
+        "type": "text",
+        "text": {
+            "preview_url": preview_url,
+            "body": message_content
         }
-    return json.dumps(mess, indent=4)
+    }, indent=4)
 
 
 def reaction_message(phone_number: int, msg_id: str, emoji: str):
-    mess = \
-        {
+    return json.dumps({
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": f"+91{phone_number}",
+        "type": "reaction",
+        "reaction": {
+            "message_id": msg_id,
+            "emoji": emoji
+        }
+    }, indent=4)
+
+
+def media_message(phone_number: int, media_url: str = None, media_object_id: str = None):
+    if media_url:
+        return json.dumps({
             "messaging_product": "whatsapp",
             "recipient_type": "individual",
-            "to": f"91{phone_number}",
-            "type": "reaction",
-            "reaction": {
-                "message_id": msg_id,
-                "emoji": emoji
+            "to": f"+91{phone_number}",
+            "type": "image",
+            "image": {
+                "link": media_url
             }
+        }, indent=4)
+    elif media_object_id:
+        return json.dumps({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": f"+91{phone_number}",
+            "type": "image",
+            "image": {
+                "id": media_object_id
+            }
+        }, indent=4)
+
+
+def location_message(phone_number: int, longitude_no: str, latitude_no: str, location_name: str, location_address: str):
+    return json.dumps({
+        "messaging_product": "whatsapp",
+        "to": f"+91{phone_number}",
+        "type": "location",
+        "location": {
+            "longitude": longitude_no,
+            "latitude": latitude_no,
+            "name": location_name,
+            "address": location_address
         }
-    return json.dumps(mess, indent=4)
-
-
-def image_message(phone_number: int, image_url: str = None, ):
-    if image_url:
-        mess = \
-            {
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": f"91{phone_number}",
-                "type": "image",
-                "image": {
-                    "link": image_url
-                }
-            }
-        return json.dumps(mess, indent=4)
-
-
-print(image_message(7992243804, "https://upload.wikimedia.org/wikipedia/commons/2/27/FG_1400_%C3%97_1400.jpg"))
+    }, indent=4)
